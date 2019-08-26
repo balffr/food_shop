c254443c69c34d2316a645823e389bbfb36fddf6
balffr <lbalffrl@mail.com>
2019-08-26 07:47:08 +0300

    part2.1

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,9 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           title: Text('Welcome to food shop'),
+          actions: <Widget>[
+            IconButton(icon: Icon(Icons.shopping_cart))
+          ],
         ),
         body: Center(child: RandomWords()),
       ),
@@ -25,21 +28,29 @@ class RandomWords extends StatefulWidget {
 }
 
 class _RandomWordsState extends State<RandomWords> {
+  final List<WordPair> _suggestions = <WordPair>[];
+  final Set<WordPair> _saved = Set<WordPair>();
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  Widget _buildRow(WordPair pair) {
+    final bool alreadySaved = _saved.contains(pair);
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+      trailing: Icon(alreadySaved ? Icons.favorite : Icons.favorite_border,
+          color: alreadySaved ? Colors.red : null),
+      onTap: () {
+        setState(() {
+          alreadySaved ? _saved.remove(pair) : _saved.add(pair);
+        });
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    final _suggestions = <WordPair>[];
-    final _biggerFont = const TextStyle(fontSize: 18.0);
-    final wordPair = WordPair.random();
-
-    Widget _buildRow(WordPair pair) {
-      return ListTile(
-        title: Text(
-          pair.asPascalCase,
-          style: _biggerFont,
-        ),
-      );
-    }
-
     Widget _buildSuggestions() {
       return ListView.builder(
         padding: const EdgeInsets.all(16.0),
